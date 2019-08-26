c452e287354366b170c6ac58fc281cc290f17f1f
wanghui <793147654@qq.com>
2019-08-26 20:32:41 +0800

    猜数字完成

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -13,7 +13,14 @@ public class Main {
 		
 		for (int i = 0; i < RUN_TIMES; i++) {
 			String input = new GuessInputCommand().input();
-			Game.playGame(input);
+			String result = Game.playGame(input);
+			if(result.equals("4A0B")) {
+				System.out.println("胜利，一切正确");
+				break;
+			}
+			else {
+				System.out.println(result+"\t错误，请重新输入");
+			}
 		}
 		System.out.println("游戏结束！");
 	}
